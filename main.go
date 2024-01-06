eabbe95d5fef0e0d256ef1999d61790e20b82305
arnab <arnabthakuria678@gmail.com>
2024-01-06 18:02:16 +0530

    initialized the project

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/rivo/tview"
+
+func main() {
+	box := tview.NewBox().SetBorder(true).SetTitle("Terminal Tetris")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
+}
